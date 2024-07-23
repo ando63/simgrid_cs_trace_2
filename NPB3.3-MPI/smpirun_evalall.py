36ac428b3cfce743547590e97b83dd0693d3a087
ando63 <126371445+ando63@users.noreply.github.com>
2024-07-23 13:54:42 +0900

    Delete NPB3.3-MPI directory

--- NPB3.3-MPI/smpirun_evalall.py
+++
@@ -1,9 +0,0 @@
-from smpirun_eval import *
-
-if __name__ == "__main__":
-    basename = "crossbar_64"
-    apps = ["bt", "cg", "ep", "ft", "is", "lu", "mg", "sp"]
-    app_size = "W"
-
-    for app in apps:
-        smpirun_single(basename, app, app_size)
