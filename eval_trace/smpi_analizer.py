562f8385c8e5fde9ee70a4bfa1c2247b139a5887
ando63 <126371445+ando63@users.noreply.github.com>
2025-08-27 10:44:50 +0900

    Update smpi_analizer.py

--- eval_trace/smpi_analizer.py
+++ eval_trace/smpi_analizer.py
@@ -5,7 +5,7 @@ import re
 import matplotlib.pyplot as plt
 
 def process_csv_to_matrix_txt(input_csv_path, output_txt_path, matrix_size=64):
-    global simulation_times_full_fat
+    global simulation_times_full_fat, simulation_times_half_fat
     """
     CSVファイルからタイムスタンプを除いた行列データを抽出し、
     指定された形式でテキストファイルに保存します。
@@ -25,6 +25,7 @@ def process_csv_to_matrix_txt(input_csv_path, output_txt_path, matrix_size=64):
         df = df.iloc[:, 17:]
         print(df)
         simulation_times_full_fat = []
+        simulation_times_half_fat = []
 
         # 出力ファイルを開き、データを書き込む
         
@@ -79,11 +80,21 @@ def process_csv_to_matrix_txt(input_csv_path, output_txt_path, matrix_size=64):
 
                 # smpirun(with half.xml)
 
+                cmd = "smpirun -no-privatize -replay traf_matrix/matrix.txt_1 --log=replay.thresh:critical --log=no_loc -np 64 -platform xmlfiles/link_config.yaml_fat_treemesh_cs_file.xml ./replay/smpi_replay --log=smpi_config.thres:warning --log=xbt_cfg.thres:warning > Output_2.txt 2>&1"
+
+                subprocess.run(cmd, shell=True, check=True)
+
                 # 結果append
 
-
-
-
+                with open("Output_2.txt", "r") as f:
+                    lines = f.readlines()
+                
+                for line in lines:
+                    # "Simulation time <数字>" を探す
+                    match = re.search(r"Simulation time (\d+\.\d+)", line)
+                    if match:
+                        # 数字部分を float に変換してリストに追加
+                        simulation_times_half_fat.append(float(match.group(1)))
 
                 # f.close()  # 一旦閉じる
                 open(output_txt_path, 'w').close()  # 中身を空にする
@@ -101,7 +112,7 @@ def process_csv_to_matrix_txt(input_csv_path, output_txt_path, matrix_size=64):
 
 # スクリプトの実行例
 if __name__ == "__main__":
-    global simulation_times_full_fat
+    global simulation_times_full_fat, simulation_times_half_fat
     # CSVファイルのパス
     csv_file_path = "time_traffic_matrix.csv"
     # 出力テキストファイルのパス
@@ -116,7 +127,11 @@ if __name__ == "__main__":
 
     print(simulation_times_full_fat)
     plt.figure(figsize=(12, 4))
-    plt.plot(simulation_times_full_fat, marker='o', markersize=3, linestyle='-')
+    # plt.plot(simulation_times_full_fat, marker='o', markersize=3, linestyle='-')
+    plt.plot(simulation_times_full_fat, label="Full Fat", marker='o', markersize=3)
+
+    # half_fat のプロット
+    plt.plot(simulation_times_half_fat, label="Half Fat", marker='x', markersize=3)
     plt.xlabel('Index')
     plt.ylabel('Value')
     plt.title('Array values from left to right')
