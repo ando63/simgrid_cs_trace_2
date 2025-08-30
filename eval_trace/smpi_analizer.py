0d7e9e15ecdfaf6813d308d3050052368fe50365
ando63 <126371445+ando63@users.noreply.github.com>
2025-08-30 12:57:22 +0900

    Update smpi_analizer.py

--- eval_trace/smpi_analizer.py
+++ eval_trace/smpi_analizer.py
@@ -4,7 +4,7 @@ import subprocess
 import re
 import matplotlib.pyplot as plt
 
-def process_csv_to_matrix_txt(input_csv_path, output_txt_path, matrix_size=64):
+def process_csv_to_matrix_txt(input_csv_path, output_txt_path, output_txt_path_2, matrix_size=64):
     global simulation_times_full_fat, simulation_times_half_fat
     """
     CSVファイルからタイムスタンプを除いた行列データを抽出し、
@@ -22,7 +22,7 @@ def process_csv_to_matrix_txt(input_csv_path, output_txt_path, matrix_size=64):
         df = pd.read_csv(input_csv_path, header=None, sep=',', skiprows=1)
 
         # 最初の列（タイムスタンプ）を削除
-        df = df.iloc[:, 17:]
+        df = df.iloc[:, 1:]
         print(df)
         simulation_times_full_fat = []
         simulation_times_half_fat = []
@@ -30,80 +30,93 @@ def process_csv_to_matrix_txt(input_csv_path, output_txt_path, matrix_size=64):
         # 出力ファイルを開き、データを書き込む
         
         with open(output_txt_path, 'w') as f:
-            # 1行ずつ行列データを取り出す
-            for _, row in df.iterrows():
-                print(row)
-                # 行のデータをNumPy配列に変換
-                matrix_data = np.array(row.values)
-
-                matrix_data[matrix_data < 0] = 0
-
-                # 1次元配列を行列の形（例: 4x4）にreshape
-                reshaped_matrix = matrix_data.reshape(matrix_size, matrix_size)
-
-                # 行列データをファイルに書き込む
-                for i in range(matrix_size):
-                    # 各行の要素をスペース区切りで文字列化
-                    row_str = ' '.join(map(str, reshaped_matrix[i]))
-                    f.write(row_str + '\n')
-                
-                # 各行列の間に空行を挿入して見やすくする
-                f.write('\n')
-                f.close()
-
-                # gen_traf
-
-                subprocess.run(
-                    ["python3", "gen_traffic.py", "matrix.txt", "1", "traf_matrix/"],
-                    check=True
-                )
-
-                # smpirun(with full.xml)
-
-                cmd = "smpirun -no-privatize -replay traf_matrix/matrix.txt_1 --log=replay.thresh:critical --log=no_loc -np 64 -platform xmlfiles/link_config.yaml_fat_treemesh_cs_file.xml ./replay/smpi_replay --log=smpi_config.thres:warning --log=xbt_cfg.thres:warning > Output.txt 2>&1"
-
-                subprocess.run(cmd, shell=True, check=True)
-
-                # 結果append
-
-                with open("Output.txt", "r") as f:
-                    lines = f.readlines()
-                
-                for line in lines:
-                    # "Simulation time <数字>" を探す
-                    match = re.search(r"Simulation time (\d+\.\d+)", line)
-                    if match:
-                        # 数字部分を float に変換してリストに追加
-                        simulation_times_full_fat.append(float(match.group(1)))
-
-
-
-                # smpirun(with half.xml)
-
-                cmd = "smpirun -no-privatize -replay traf_matrix/matrix.txt_1 --log=replay.thresh:critical --log=no_loc -np 64 -platform xmlfiles/link_config.yaml_half_fat_treemesh_cs_file.xml ./replay/smpi_replay --log=smpi_config.thres:warning --log=xbt_cfg.thres:warning > Output_2.txt 2>&1"
-
-                subprocess.run(cmd, shell=True, check=True)
-
-                # 結果append
-
-                with open("Output_2.txt", "r") as f:
-                    lines = f.readlines()
-                
-                for line in lines:
-                    # "Simulation time <数字>" を探す
-                    match = re.search(r"Simulation time (\d+\.\d+)", line)
-                    if match:
-                        # 数字部分を float に変換してリストに追加
-                        simulation_times_half_fat.append(float(match.group(1)))
-
-                # f.close()  # 一旦閉じる
-                open(output_txt_path, 'w').close()  # 中身を空にする
-
-                # 🔽 また追記モードで開き直す
-                f = open(output_txt_path, 'a')
-        
-
-        print(f"データの処理が完了しました。ファイル '{output_txt_path}' に保存されました。")
+            with open(output_txt_path_2, 'w') as f2:
+                # 1行ずつ行列データを取り出す
+                for _, row in df.iterrows():
+                    print(row)
+                    # 行のデータをNumPy配列に変換
+    
+                    first_16_elements = row[:16]
+                    remaining_elements = row[16:]
+                    matrix_data_4_4 = np.array(first_16_element.values)
+                    matrix_data_4_4[matrix_data_4_4 < 0] = 0
+    
+                    matrix_data_64_64 = np.array(remaining_element.values)
+                    matrix_data_64_64[matrix_data_64_64 < 0] = 0
+    
+                    # 1次元配列を行列の形（例: 4x4）にreshape
+                    reshaped_matrix_4_4 = matrix_data_4_4.reshape(4, 4)
+                    reshaped_matrix_64_64 = matrix_data_64_64.reshape(64,64)
+    
+                    # 行列データをファイルに書き込む
+                    for i in range(4):
+                        # 各行の要素をスペース区切りで文字列化
+                        row_str = ' '.join(map(str, reshaped_matrix_4_4[i]))
+                        f.write(row_str + '\n')
+    
+                    for i in range(64):
+                        row_str = ' '.join(map(str, reshaped_matrix_64_64[i]))
+                        f2.write(row_str + '\n')
+                    
+                    # 各行列の間に空行を挿入して見やすくする
+                    f.write('\n')
+                    f2.write('\n')
+                    f.close()
+                    f2.close()
+    
+                    # gen_traf
+    
+                    subprocess.run(
+                        ["python3", "gen_traffic.py", "matrix.txt", "1", "traf_matrix/"],
+                        check=True
+                    )
+    
+                    # smpirun(with full.xml)
+    
+                    cmd = "smpirun -no-privatize -replay traf_matrix/matrix.txt_1 --log=replay.thresh:critical --log=no_loc -np 64 -platform xmlfiles/link_config.yaml_fat_treemesh_cs_file.xml ./replay/smpi_replay --log=smpi_config.thres:warning --log=xbt_cfg.thres:warning > Output.txt 2>&1"
+    
+                    subprocess.run(cmd, shell=True, check=True)
+    
+                    # 結果append
+    
+                    with open("Output.txt", "r") as f:
+                        lines = f.readlines()
+                    
+                    for line in lines:
+                        # "Simulation time <数字>" を探す
+                        match = re.search(r"Simulation time (\d+\.\d+)", line)
+                        if match:
+                            # 数字部分を float に変換してリストに追加
+                            simulation_times_full_fat.append(float(match.group(1)))
+    
+    
+    
+                    # smpirun(with half.xml)
+    
+                    cmd = "smpirun -no-privatize -replay traf_matrix/matrix.txt_1 --log=replay.thresh:critical --log=no_loc -np 64 -platform xmlfiles/link_config.yaml_half_fat_treemesh_cs_file.xml ./replay/smpi_replay --log=smpi_config.thres:warning --log=xbt_cfg.thres:warning > Output_2.txt 2>&1"
+    
+                    subprocess.run(cmd, shell=True, check=True)
+    
+                    # 結果append
+    
+                    with open("Output_2.txt", "r") as f:
+                        lines = f.readlines()
+                    
+                    for line in lines:
+                        # "Simulation time <数字>" を探す
+                        match = re.search(r"Simulation time (\d+\.\d+)", line)
+                        if match:
+                            # 数字部分を float に変換してリストに追加
+                            simulation_times_half_fat.append(float(match.group(1)))
+    
+                    # f.close()  # 一旦閉じる
+                    open(output_txt_path, 'w').close()  # 中身を空にする
+    
+                    # 🔽 また追記モードで開き直す
+                    f = open(output_txt_path, 'a')
+            
+    
+            print(f"データの処理が完了しました。ファイル '{output_txt_path}' に保存されました。")
 
     except FileNotFoundError:
         print(f"エラー: ファイル '{input_csv_path}' が見つかりませんでした。パスを確認してください。")
@@ -117,13 +130,14 @@ if __name__ == "__main__":
     csv_file_path = "time_traffic_matrix.csv"
     # 出力テキストファイルのパス
     txt_file_path = "traf_matrix/matrix.txt"
+    txt_file_path_2 = "traf_matrix/matrix_2.txt"
     
     # 実際に行列のサイズに合わせて変更してください
     # 例：4x4行列の場合は4
     matrix_dimension = 64 
     print(matrix_dimension)
 
-    process_csv_to_matrix_txt(csv_file_path, txt_file_path, matrix_dimension)
+    process_csv_to_matrix_txt(csv_file_path, txt_file_path, txt_file_path_2, matrix_dimension)
 
     print(simulation_times_full_fat)
     plt.figure(figsize=(12, 4))
